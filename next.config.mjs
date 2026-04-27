b286b8fb6310af6326cf98728d5b795d91f3963b
vignesh-poovanna <vigneshpoovannaas@gmail.com>
2026-04-28 00:08:42 +0530

    chore: remove basePath for custom domain root deployment

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,6 @@ const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  basePath,
   images: { unoptimized: true },
-  env: {
-    NEXT_PUBLIC_BASE_PATH: basePath,
-  },
 };
 export default nextConfig;
